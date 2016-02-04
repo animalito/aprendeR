4a03b324308ef6593a9d111e941aa14d5b076dd9
animalito <animalsalvaje@gmail.com>
2016-02-04 17:17:51 -0600

    vectores

--- lecture_01/1_basico_main.sh
+++ lecture_01/1_basico_main.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Rscript 1_basico_render.r
+# xelatex 1_basico.tex
+# biber 1_basico
+# xelatex 1_basico.tex
+# xelatex 1_basico.tex
+
 Rscript 1_basico_render.r
-xelatex 1_basico.tex
+pdflatex 1_basico.tex
 biber 1_basico
-xelatex 1_basico.tex
-xelatex 1_basico.tex
+pdflatex 1_basico.tex
+pdflatex 1_basico.tex
