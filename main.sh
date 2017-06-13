f3dfe3237c9e46c0b0a7bae0d121d24ba8bdf4f2
Andrea Fernandez <animalsalvaje@gmail.com>
2017-06-13 11:48:20 -0500

    cambios coatlicue

--- main.sh
+++ main.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd 00_otros/
-./otros_main.sh
-cd ../01_programacion_basica/
-./1_basico_main.sh
-./1b_basico_main.sh
-./2_basico_main.sh
-cd ../02_manipulacion/
-./manipulacion_main.sh
-cd ../03_visualizacion/
-./viz_main.sh
-
-cd ..
+# cd 00_otros/
+# ./otros_main.sh
+# cd ../01_programacion_basica/
+# ./1_basico_main.sh
+# ./1b_basico_main.sh
+# ./2_basico_main.sh
+# cd ../02_manipulacion/
+# ./manipulacion_main.sh
+# cd ../03_visualizacion/
+# ./viz_main.sh
+# 
+# cd ..
 pdflatex main.tex
 biber main
 pdflatex main.tex
